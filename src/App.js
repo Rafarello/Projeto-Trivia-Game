62e7864b610805e53c855d6c79a40c31e8cc12e9
pereiravinao <57878050+pereiravinao@users.noreply.github.com>
2021-10-21 09:52:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import Content from './componets/content';
 
 export default function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/tela-de-jogo" component={ TelaDeJogo } />
-        <Route path="/settings" component={ Settings } />
-        <Route exact path="/" component={ Login } />
-      </Switch>
-    </div>
+   <Content />
   );
 }

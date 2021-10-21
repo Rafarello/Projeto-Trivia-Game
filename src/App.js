83f94cc540c90d8da713494e0d3c72ef4bce03ac
vinicius-mns <vinicius_mns@hotmail.com>
2021-10-21 09:00:50 -0300

    Css login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
-import TelaDeJogo from './pages/TelaDeJogo';
-import Settings from './pages/Settings';
+import Content from './componets/content';
 
 export default function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/tela-de-jogo" component={ TelaDeJogo } />
-        <Route exact path="/" component={ Login } />
-        <Route path="/settings" component={ Settings } />
-      </Switch>
-    </div>
+    <Content />
   );
 }
